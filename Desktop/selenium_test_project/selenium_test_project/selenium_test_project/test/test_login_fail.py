e6fe52195c4003cf0fb7d8de447718a9da17998c
Arisluz <166089200+Arisluz@users.noreply.github.com>
2025-04-11 20:51:43 -0400

    Update test_login_fail.py

--- Desktop/selenium_test_project/selenium_test_project/selenium_test_project/test/test_login_fail.py
+++ Desktop/selenium_test_project/selenium_test_project/selenium_test_project/test/test_login_fail.py
@@ -2,13 +2,13 @@ def test_login_invalid_credentials(driver):
     driver.get("https://buggy.justtestit.org/")
     time.sleep(2)
 
-    wait = WebDriverWait(driver, 30)  # Aumentamos a 30 segundos
+    wait = WebDriverWait(driver, 30)  
 
-    # Cambiamos la espera de visibilidad de los campos
+    
     wait_and_type(driver, By.ID, "user", "wronguser", wait, 1)
     wait_and_type(driver, By.ID, "password", "wrongpassword", wait, 1)
 
-    # Aseguramos que el botón sea clickeable antes de hacer click
+    
     login_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))
     login_button.click()
     time.sleep(2)
