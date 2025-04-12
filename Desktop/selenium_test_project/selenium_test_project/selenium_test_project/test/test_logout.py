f2f4d97b7782443fb034afdec33f69a775310658
Arisluz <166089200+Arisluz@users.noreply.github.com>
2025-04-11 20:52:20 -0400

    Update test_logout.py

--- Desktop/selenium_test_project/selenium_test_project/selenium_test_project/test/test_logout.py
+++ Desktop/selenium_test_project/selenium_test_project/selenium_test_project/test/test_logout.py
@@ -1,8 +1,8 @@
 def test_logout(driver):
     driver.get("https://buggy.justtestit.org/")
-    wait = WebDriverWait(driver, 30)  # Aumentamos a 30 segundos
+    wait = WebDriverWait(driver, 30)  
 
-    # Login primero
+  
     username_field = wait.until(EC.visibility_of_element_located((By.ID, "user")))
     password_field = wait.until(EC.visibility_of_element_located((By.ID, "password")))
     login_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))
@@ -11,11 +11,11 @@ def test_logout(driver):
     password_field.send_keys("Selenium123!")
     login_button.click()
 
-    # Esperamos que el botón de logout esté visible
+   
     logout_button = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Logout")))
     logout_button.click()
 
-    # Aseguramos que la URL haya cambiado correctamente
+   
     assert driver.current_url == "https://buggy.justtestit.org/"
 
     driver.save_screenshot("screenshots/logout_success.png")
